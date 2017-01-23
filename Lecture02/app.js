ad9f7f132b9d6369635a454dca34dc56708f678e
Sergio Muñoz <smunoz@ayt.cl>
2017-01-23 16:06:44 -0300

    Lecture02

---
+++ Lecture02/app.js
@@ -0,0 +1,25 @@
+var student = {
+  name : "",
+  type : "student"
+};
+
+document.addEventListener('DOMContentLoaded', contentLoad);
+
+function contenLoaded(event){
+  document.getElementById('name').addEventListener("keyup", keyup);
+}
+
+function keyup(event){
+  calculateNumericOutput();
+}
+function calculateNumericOutput(){
+  student.name = document.getElementById('name').value;
+
+  var totalNameValue=0;
+  for (var i = 0; i < student.name.length; i++) {
+    totalNameValue +=student.name.charCodeAt(i);
+  }
+  //insertando el resultado en la pagina
+  var output = "Total numerico del nombre es " + totalNameValue;
+  document.getElementById('output').innerText = output;
+}
